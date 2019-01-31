db48c35af48cb58c60cb2fdb0ccdea8e62b7f749
Omar Mohamed <omar.mohamed.latif@gmail.com>
2019-01-31 21:45:01 +0200

    -added download data script -added extract data script

---
+++ DownloadData.py
@@ -0,0 +1,47 @@
+
+import os
+import sys
+from six.moves.urllib.request import urlretrieve
+
+
+url = 'http://opus.nlpl.eu/download.php?f=MultiUN/v1/moses/'
+last_percent_reported = None
+data_root = '.'  # Change me to store data elsewhere
+
+
+
+def download_progress_hook(count, blockSize, totalSize):
+    """A hook to report the progress of a download. This is mostly intended for users with
+    slow internet connections. Reports every 5% change in download progress.
+    """
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+        last_percent_reported = percent
+
+
+def maybe_download(filename, expected_bytes, force=False):
+    """Download a file if not present, and make sure it's the right size."""
+    dest_filename = os.path.join(data_root, filename)
+    if force or not os.path.exists(dest_filename):
+        print('Attempting to download:', filename)
+        filename, _ = urlretrieve(url + filename, dest_filename, reporthook=download_progress_hook)
+        print('\nDownload Complete!')
+    statinfo = os.stat(dest_filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', dest_filename)
+    else:
+        raise Exception(
+            'Failed to verify ' + dest_filename + '. Can you get to it with a browser?')
+    return dest_filename
+
+
+train_filename = maybe_download('ar-en.txt.zip', 1036296368)
---
+++ ExtractData.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import tarfile
+
+data_root = '.'
+
+def maybe_extract(filename, force=False):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    if os.path.isdir(root) and not force:
+        # You may override by setting force=True.
+        print('%s already present - Skipping extraction of %s.' % (root, filename))
+    else:
+        print('Extracting data for %s. This may take a while. Please wait.' % root)
+        tar = tarfile.open(filename)
+        sys.stdout.flush()
+        tar.extractall(data_root)
+        tar.close()
+
+
+train_filename = os.path.join(data_root, 'ar-en.txt.zip')
+
+
+
+maybe_extract(train_filename)
